281c2e8ca41c4da2a169955db51bfcea8e126e1f
Dormailler <gerrerd@naver.com>
2023-11-11 03:48:27 +0900

    일정 관리 및 그룹 찾기 사이트

--- whenmeet/src/main/java/com/project/wm/WhenmeetApplication.java
+++ whenmeet/src/main/java/com/project/wm/WhenmeetApplication.java
@@ -3,15 +3,21 @@ package com.project.wm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"main","login","schedule","meeting","search","group","websocket"})
 @MapperScan(basePackages = {"main","login","schedule","meeting","search","group"})
-public class WhenmeetApplication {
-
+public class WhenmeetApplication extends SpringBootServletInitializer{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WhenmeetApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WhenmeetApplication.class);
 	}
 
 }
